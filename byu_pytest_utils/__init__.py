0d3d9ffd7fecf75a4136071a427e7a2e10e8f5d2
Gordon Bean <gbean@cs.byu.edu>
2023-05-18 16:45:29 -0600

    add example usage in test

--- byu_pytest_utils/__init__.py
+++ byu_pytest_utils/__init__.py
@@ -7,7 +7,7 @@ pytest.register_assert_rewrite("byu_pytest_utils.dialog")
 
 from .utils import run_python_script, with_import, ensure_missing
 from .decorators import max_score, visibility, tags
-from .dialog import dialog
+from .dialog import dialog, dialog_exec
 
 # Type stubs to make these variables discoverable
 # Actual values are supplied by __getattr__
---
+++ tests/test_dialog_exec_framework.py
@@ -0,0 +1,18 @@
+from functools import cache
+from byu_pytest_utils import dialog_exec
+
+# This file demonstrates how we can use
+#  predefined assets (like compiled binaries)
+#  in the dialog framework
+
+@cache
+def binary():
+    print('g++ -o ./exec main.cpp')
+    return './exec'
+
+@dialog_exec(
+    'example.dialog.txt',
+    binary, 'arg1', 'arg2'
+)
+def test_binary(): ...
+
