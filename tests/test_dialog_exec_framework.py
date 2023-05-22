f1d6a78689d2ea41455c5fbf5de67cc97b8ca06e
sb64 <53383020+sb64@users.noreply.github.com>
2023-05-22 12:13:18 -0600

    add black box tests

--- tests/test_dialog_exec_framework.py
+++ tests/test_dialog_exec_framework.py
@@ -1,17 +1,73 @@
-from byu_pytest_utils import dialog_exec, cache
+from byu_pytest_utils import dialog_exec, max_score
 
-# This file demonstrates how we can use
-#  predefined assets (like compiled binaries)
-#  in the dialog framework
-
-@cache
-def binary():
-    print('g++ -o ./exec main.cpp')
-    return './exec'
 
 @dialog_exec(
-    'example.dialog.txt',
-    binary, 'arg1', 'arg2'
+    "dialogs/test_dialog_should_pass.txt",
+    "python3", "script_for_dialog_passes.py", 'woot', 7
 )
-def test_binary(): ...
+@max_score(10)
+def test_dialog_should_pass():
+    """Everything should pass"""
 
+
+@dialog_exec(
+    "dialogs/test_dialog_should_pass.txt",
+    "python3", "script_for_dialog_fails.py", 'woot', 7, 'foobar'
+)
+@max_score(10)
+def test_dialog_should_fail():
+    """
+    seven should pass, but another-number and everything-else should fail
+    """
+
+
+@dialog_exec(
+    "dialogs/test_dialog_expects_more_input.txt",
+    "python3", "script_for_dialog_passes.py", 'woot'
+)
+@max_score(10)
+def test_dialog_expects_more_input_should_fail():
+    """
+    args, seven, and eight should pass, 
+    but nine and everything-else should not
+    """
+
+
+@max_score(10)
+@dialog_exec(
+    "dialogs/test_dialog_expects_less_input.txt",
+    "python3", "script_for_dialog_passes.py", 'woot'
+)
+def test_dialog_expects_less_input_should_fail():
+    """
+    seven should pass, but everything-else should fail
+    There should be an error: "Input called more times than expected"
+    """
+
+
+@max_score(10)
+@dialog_exec(
+    "dialogs/basic_text_output.expected.txt",
+    "python3", "script_that_writes_to_file.py",
+    "dialogs/basic_text_input_file.txt",
+    "basic_text_output.observed.txt",
+    output_file="basic_text_output.observed.txt"
+)
+def test_dialog_output_file():
+    """
+    Should pass cleanly
+    """
+
+
+@max_score(10)
+@dialog_exec(
+    "dialogs/basic_text_output.dialog.expected.txt",
+    "python3", "script_that_writes_to_file.py",
+    "dialogs/basic_text_input_file.txt",
+    "basic_text_output.observed.txt",
+    output_file="basic_text_output.observed.txt"
+)
+def test_dialog_output_file_groups():
+    """
+    Should match with partial-credit groups
+    """
