04cbfabb97844e6451607ac2fdb59a8fbe4015bc
Gordon Bean <gbean@cs.byu.edu>
2023-06-06 15:36:25 -0600

    0.6.3 - fix error reporting for dialog output file not found

--- byu_pytest_utils/dialog.py
+++ byu_pytest_utils/dialog.py
@@ -221,17 +221,19 @@ class DialogChecker:
         try:
             runpy.run_path(script_name, _globals, module)
 
+            if output_file is not None:
+                if os.path.exists(output_file):
+                    with open(output_file) as output:
+                        group_stats = self._score_output(output.read())
+                else:
+                    group_stats = self._score_output(f"File not found: {output_file}. Did you write it?")
+            else:
+                group_stats = self._score_output(self.observed_output)
+
         except Exception as ex:
             # get stack trace as string
-            self._consume_output(f'\nException: {ex}\n')
-            self._consume_output(traceback.format_exc())
-
-        # Final assertion of observed and expected output
-        if output_file is not None:
-            with open(output_file) as output:
-                group_stats = self._score_output(output.read())
-        else:
-            group_stats = self._score_output(self.observed_output)
+            exception = f"Exception: {ex}\n{traceback.format_exc()}"
+            group_stats = self._score_output(exception)
 
         return group_stats
 
